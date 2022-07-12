1257af58bb7ba82791ad45602a1e50d1743e8514
Carlos Guimaraes <sekmet@open4g.com>
2022-07-12 00:28:18 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -405,8 +405,8 @@ export default function Example() {
           <h2 id="footer-heading" className="sr-only">
             Footer
           </h2>
-          <div className="max-w-md mx-auto pt-12 px-4 sm:max-w-7xl sm:px-6 lg:pt-16 lg:px-8">
-            {/*<div className="xl:grid xl:grid-cols-3 xl:gap-8">*/}
+{/*<div className="max-w-md mx-auto pt-12 px-4 sm:max-w-7xl sm:px-6 lg:pt-16 lg:px-8">
+            {/*<div className="xl:grid xl:grid-cols-3 xl:gap-8">* /}
             <div className="xl:grid xl:grid-cols-2 xl:gap-8">
               <div className="space-y-8 xl:col-span-1">
                 <img
@@ -425,7 +425,7 @@ export default function Example() {
                     </a>
                   ))}
                 </div>
-              </div>
+              </div>*/}
                {/*<div className="mt-12 grid grid-cols-2 gap-8 xl:mt-0 xl:col-span-2">
 
                <div className="md:grid md:grid-cols-2 md:gap-8">
